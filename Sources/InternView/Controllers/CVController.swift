ad77c93d0f4f5869ea01c113f3ca992e8630281b
oartemiy <artemiy.obraztsov@yandex.ru>
2026-02-01 12:43:39 +0300

    it's working

--- Sources/InternView/Controllers/CVController.swift
+++ Sources/InternView/Controllers/CVController.swift
@@ -1,8 +1,24 @@
 //
-//  File.swift
+//  CVController.swift
 //  InternView
 //
 //  Created by Артемий Образцов on 01.02.2026.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+struct CVController: RouteCollection {
+    func boot(routes: any Vapor.RoutesBuilder) throws {
+        let cvGroup = routes.grouped("CVs")
+        cvGroup.post(use: createHandler)
+    }
+    
+    func createHandler(_ req: Request) async throws -> Response {
+        guard let cv = try? req.content.decode(CV.self) else {
+            throw Abort(.badRequest, reason: "It is impossible to decode the CV model")
+        }
+        try await cv.save(on: req.db)
+        return try await cv.encodeResponse(status: .created, for: req)
+    }
+}
--- Sources/InternView/Models/CV.swift
+++ Sources/InternView/Models/CV.swift
@@ -9,7 +9,6 @@ import Fluent
 import Vapor
 
 final class CV: Model, Content, @unchecked Sendable {
-
     static let schema: String = "CVs"
 
     @ID(key: .id)
@@ -22,16 +21,16 @@ final class CV: Model, Content, @unchecked Sendable {
     var description: String
 
     @Field(key: "user_id")
-    var userId: UUID?
+    var user_id: String
 
-    @Timestamp(key: "created_at", on: .create, format: .iso8601)
-    var createdAt: Date?
-
-    @Timestamp(key: "updated_at", on: .update, format: .iso8601)
-    var updatedAt: Date?
-
-    @Timestamp(key: "deleted_at", on: .delete, format: .iso8601)
-    var deletedAt: Date?
+//    @Timestamp(key: "created_at", on: .create, format: .iso8601)
+//    var createdAt: Date?
+//
+//    @Timestamp(key: "updated_at", on: .update, format: .iso8601)
+//    var updatedAt: Date?
+//
+//    @Timestamp(key: "deleted_at", on: .delete, format: .iso8601)
+//    var deletedAt: Date?
 
     @Field(key: "pdf")
     var pdf: String
@@ -42,19 +41,13 @@ final class CV: Model, Content, @unchecked Sendable {
         id: UUID? = nil,
         title: String,
         description: String,
-        userId: UUID? = nil,
-        createdAt: Date? = nil,
-        updatedAt: Date? = nil,
-        deletedAt: Date? = nil,
+        user_id: String,
         pdf: String
     ) {
         self.id = id
         self.title = title
         self.description = description
-        self.userId = userId
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-        self.deletedAt = deletedAt
+        self.user_id = user_id
         self.pdf = pdf
     }
 
