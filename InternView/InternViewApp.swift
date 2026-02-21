b2cdecbb53135e635fddc06590bb5a75494a8f3a
oartemiy <artemiy.obraztsov@yandex.ru>
2026-02-21 22:42:49 +0300

    Initial Commit

---
+++ InternView/InternViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternViewApp.swift
+//  InternView
+//
+//  Created by Артемий Образцов on 21.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct InternViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
